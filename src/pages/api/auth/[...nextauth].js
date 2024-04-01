96aafb8b6c44ea871683c1907c63302e7993cc52
gusadolfo16 <73118764+gusadolfo16@users.noreply.github.com>
2024-04-01 22:27:04 +0000

    cambio en el FirebaseAdapter

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { firebaseConfig } from "../../../firebaseConfig"; // Import Firebase config
-import { adapter } from "@next-auth/firebase-adapter/firebaseAdapter";
+import { FirebaseAdapter } from "@next-auth/firebase-adapter";
 
 export default NextAuth({
   providers: [
@@ -10,8 +10,8 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       redirectUri: process.env.GOOGLE_REDIRECT_URL, // Replace with your domain
     }),
-    // You can add other providers here
+    // You can add other providers here (e.g., Facebook, GitHub)
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  adapter: adapter(firebaseConfig), // Use adapter with firebaseConfig
+  adapter: FirebaseAdapter(firebaseConfig), // Use adapter directly with firebaseConfig
 });
