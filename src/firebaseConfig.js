872bf1769aa67abdad04e6399aeaf243e6121115
gusadolfo16 <73118764+gusadolfo16@users.noreply.github.com>
2024-04-01 19:01:54 +0000

    variables de ambiente

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,14 +5,14 @@ import 'firebase/compat/firestore';
 
 // firebaseConfig.js
 const firebaseConfig = {
-    apiKey: "AIzaSyDnNUquLvpubs0mmPOuDV1qYL-RQHHyVGE",
-    authDomain: "parker-7bf18.firebaseapp.com",
-    databaseURL: "https://parker-7bf18-default-rtdb.firebaseio.com",
-    projectId: "parker-7bf18",
-    storageBucket: "parker-7bf18.appspot.com",
-    messagingSenderId: "486979200513",
-    appId: "1:486979200513:web:36cb351af0ac77df0a4200",
-    measurementId: "G-TGRH3ZB7V3"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
   if (!firebase.apps.length) {
